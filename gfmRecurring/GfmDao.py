b525e90eda14b9d8ea4b7b7ecbc4bf52f6ed8ffb
njain <njain@hubspot.com>
2022-01-15 02:00:07 +0530

    adding comments for the methods

--- gfmRecurring/GfmDao.py
+++ gfmRecurring/GfmDao.py
@@ -8,17 +8,36 @@ class GfmDao:
         self.campaign_data = {}
         self.donor_data = {}
 
+    """ 
+    This method adds donor
+    :param donor_name name of the donor to be added
+    :param limit_amt limit amount donor would be paying
+    :return None
+    """
     def add_donor(self, donor_name, limit_amt):
         donor = Donor(donor_name, int(limit_amt[1:]), 0)
         self.donor_data[donor_name] = donor
 
+    """ 
+    This method adds campaign
+    :param campaign_name name of the campaign to be added
+    :return None
+    """
     def add_campaign(self, campaign_name):
         campaign = Campaign(campaign_name, 0)
         self.campaign_data[campaign_name] = campaign
 
+    """ 
+    This method returns all the donors
+    :return dictionary of donors
+    """
     def get_all_donors(self):
         return self.donor_data
 
+    """ 
+    This method returns all the campaigns
+    :return dictionary of campaigns
+    """
     def get_all_campaigns(self):
         return self.campaign_data
 
--- gfmRecurring/ProcessCommands.py
+++ gfmRecurring/ProcessCommands.py
@@ -4,6 +4,14 @@ class ProcessCommands:
     def __init__(self, gfm_dao):
         self.gfm_dao = gfm_dao
 
+    """ 
+    This method parses and processes commands based on operation and entity
+    operations : Add, Donate
+    entities : Donor, Campaign
+    It throws an error if operation or entity is not valid
+    :param command_list list of all commands submitted
+    :return None
+    """
     def parse_and_process_commands(self, command_list: list):
         for i in command_list:
             command = i.strip('\n').split(" ")
@@ -19,6 +27,15 @@ class ProcessCommands:
             else:
                 raise ValueError("Commands not recognized")
 
+    """ 
+    This method processes donations
+    it throws error if the donor is not already added,
+    it skips the donation if the limit is already meet
+    :param donor_name name of the donor
+    :param campaign_name name of the campaign to which donation is being made
+    :param donation amount being donated
+    :return None
+    """
     def process_donation(self, donor_name, campaign_name, donation):
         donor, campaign = self.check_return_if_donor_and_campaign_present(donor_name, campaign_name)
         if not donor.donation_limit_meet:
@@ -28,6 +45,13 @@ class ProcessCommands:
                 donor.donation_limit_meet = True
             campaign.total_donation = campaign.total_donation + int(donation[1:])
 
+    """ 
+    Checks if the donor and campaign have been added
+    it throws error if the donor is not already added
+    :param donor_name name of the donor
+    :param campaign_name name of the campaign to which donation is being made
+    :return None
+    """
     def check_return_if_donor_and_campaign_present(self, donor_name, campaign_name):
         all_donors = self.gfm_dao.get_all_donors()
         all_campaigns = self.gfm_dao.get_all_campaigns()
@@ -37,6 +61,10 @@ class ProcessCommands:
             raise ValueError("Campaign " + campaign_name + " was not added. Please add campaign before donating.")
         return all_donors.get(donor_name), all_campaigns.get(campaign_name)
 
+    """ 
+    This method prints stats
+    :return None
+    """
     def print_stats(self):
         all_donors = self.gfm_dao.get_all_donors()
         all_campaigns = self.gfm_dao.get_all_campaigns()
